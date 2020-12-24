405a88d5f202da74def11060165de9ec261b353a
Peisen Sun <sps@snnu.edu.cn>
2020-12-24 23:42:28 +0800

    Update hanota.py

--- hanota.py
+++ hanota.py
@@ -1,17 +1,14 @@
 class Solution:
     def hanota(self, A: List[int], B: List[int], C: List[int]) -> None:
-        """
-        Do not return anything, modify C in-place instead.
-        """
         n = len(A)
         self.move(n,A,B,C)
-    def move(self,n,A,B,C):
-        if n == 1:
+    def move(self,n,A,B,C):  
+        if n == 1:                 # if n=1, move A to C
             C.append(A[-1])
             A.pop()
             return
         else:
-            self.move(n-1,A,C,B)
-            C.append(A[-1])
+            self.move(n-1,A,C,B)   # move n-1 from A to B
+            C.append(A[-1])        # move 1 from A to C
             A.pop()
-            self.move(n-1,B,A,C)
+            self.move(n-1,B,A,C)   # move n-1 from B to C
